77c4abcab03638fa4441f8c9d88430a6a4ef5a2d
Dennis Reimann <mail@dennisreimann.de>
2023-12-19 14:58:13 +0100

    Update BTCPay Server to v1.12.1

--- BTCPayServer.Plugins.PodServer/PodServerPlugin.cs
+++ BTCPayServer.Plugins.PodServer/PodServerPlugin.cs
@@ -17,7 +17,7 @@ public class PodServerPlugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        new () { Identifier = nameof(BTCPayServer), Condition = ">=1.12.0" }
+        new () { Identifier = nameof(BTCPayServer), Condition = ">=1.12.1" }
     };
 
     public override void Execute(IServiceCollection services)
